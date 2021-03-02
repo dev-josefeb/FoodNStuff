9040a1be0a1f6e46147bdac046209486e7fdc5df
dev-josefeb <febinjose@protonmail.com>
2021-03-02 19:08:43 +0100

    Added firebase config and scripts to project

---
+++ scripts/setenv.ts
@@ -0,0 +1,31 @@
+const { writeFile } = require('fs');
+const { argv } = require('yargs');
+// read environment variables from .env file
+// read the command line arguments passed with yargs
+const environment = argv.environment;
+const isProduction = environment === 'prod';
+const targetPath = isProduction ? `./src/environments/environment.prod.ts` : `./src/environments/environment.ts`;
+require('dotenv').config({ path: './.env' });
+// we have access to our environment variables
+// in the process.env object thanks to dotenv
+const environmentFileContent = `
+export const environment = {
+   production: ${isProduction},
+   firebaseConfig:{
+   apiKey: "${process.env.FIREBASE_API_KEY}",
+   authDomain: "${process.env.AUTH_DOMAIN}",
+   projectId: "${process.env.PROJECT_ID}",
+   storageBucket: "${process.env.STORAGE_BUCKET}",
+   messagingSenderId: "${process.env.MESSAGING_SENDER_ID}",
+   appId: "${process.env.APP_ID}",
+   measurementId: "${process.env.MEASUREMENT_ID}"
+   }
+};
+`;
+// write the content to the respective file
+writeFile(targetPath, environmentFileContent, function (err) {
+  if (err) {
+    console.log(err);
+  }
+  console.log(`Wrote variables to ${targetPath}`);
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from './../environments/environment';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AngularFireModule.initializeApp(environment.firebaseConfig), AngularFireDatabaseModule, AngularFireAuthModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
