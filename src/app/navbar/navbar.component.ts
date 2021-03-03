e7560f1985e8c34aebbb3b5aa4859815ad60820f
dev-josefeb <febinjose@protonmail.com>
2021-03-03 10:51:47 +0100

    Added logout and show current logged in username

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase';
 
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent implements OnInit {
-  constructor() {}
+export class NavbarComponent {
+  user: firebase.User;
 
-  ngOnInit(): void {}
+  constructor(private fireAuth: AngularFireAuth) {
+    this.fireAuth.authState.subscribe((user) => (this.user = user));
+  }
+
+  logout() {
+    this.fireAuth.signOut();
+  }
 }
