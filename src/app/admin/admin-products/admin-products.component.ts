37e9ff990d81f8cfcd98c2e3650da1d6bf60a54b
dev-josefeb <febinjose@protonmail.com>
2021-03-04 20:46:38 +0100

    Implemented product search in the table

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,24 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AppProduct } from '../../models/app-product';
 import { ProductService } from '../../product.service';
 import { map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css'],
 })
-export class AdminProductsComponent {
+export class AdminProductsComponent implements OnDestroy {
   products: AppProduct[];
+  filteredProducts: AppProduct[];
+  subscription: Subscription;
 
   constructor(private productService: ProductService) {
-    productService
+    this.subscription = productService
       .getAll()
       .snapshotChanges()
       .pipe(map((changes) => changes.map((c) => ({ key: c.payload.key, ...c.payload.val() }))))
       .subscribe((data) => {
-        this.products = data;
+        this.filteredProducts = this.products = data;
         console.log('Products are ', data);
       });
+  }
+
+  filter(query: string) {
+    this.filteredProducts = query ? this.products.filter((p: AppProduct) => p.title.toLowerCase().includes(query.toLowerCase())) : this.products;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
