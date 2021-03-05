651c94564c133fc55956de83b25d1f95ea1f0a72
dev-josefeb <febinjose@protonmail.com>
2021-03-05 18:19:31 +0100

    Refactoring: created private methods

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -5,7 +5,7 @@ import { ProductService } from '../product.service';
 import { CategoryService } from '../category.service';
 import { AppCategory } from '../models/app-category';
 import { ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-products',
@@ -15,32 +15,41 @@ import { map } from 'rxjs/operators';
 export class ProductsComponent {
   products: AppProduct[] = [];
   filteredProducts: AppProduct[] = [];
-  subscription: Subscription;
   categories: AppCategory[];
   category: string;
 
-  constructor(productService: ProductService, categoryService: CategoryService, route: ActivatedRoute) {
-    this.subscription = productService
+  constructor(private productService: ProductService, private categoryService: CategoryService, private route: ActivatedRoute) {
+    this.populateProducts();
+    this.populateCategories();
+  }
+
+  private populateProducts() {
+    this.productService
       .getAll()
       .snapshotChanges()
       .pipe(map((changes) => changes.map((c) => ({ key: c.payload.key, ...c.payload.val() }))))
       .subscribe((data) => {
         this.products = data;
-      });
 
-    categoryService
+        this.populateFilteredProducts();
+      });
+  }
+
+  private populateFilteredProducts() {
+    this.route.queryParamMap.subscribe((params) => {
+      this.category = params.get('category');
+
+      this.filteredProducts = this.category ? this.products.filter((p) => p.category === this.category) : this.products;
+    });
+  }
+
+  private populateCategories() {
+    this.categoryService
       .getAll()
       .snapshotChanges()
       .pipe(map((changes) => changes.map((c) => ({ key: c.payload.key, ...c.payload.val() }))))
       .subscribe((data) => {
         this.categories = data;
-
-        // Second Observable has to be here, otherwise empty product page intitialised in the beginning (async)
-        route.queryParamMap.subscribe((params) => {
-          this.category = params.get('category');
-
-          this.filteredProducts = this.category ? this.products.filter((p) => p.category === this.category) : this.products;
-        });
       });
   }
 }
