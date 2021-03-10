005b628018aabb385bbef9369d520ca45e151084
dev-josefeb <febinjose@protonmail.com>
2021-03-10 12:35:01 +0100

    Add product category animation

--- src/app/products/product-filter/product-filter.component.ts
+++ src/app/products/product-filter/product-filter.component.ts
@@ -1,4 +1,6 @@
+import { trigger, transition, query, stagger, animateChild, style, animate, useAnimation, group } from '@angular/animations';
 import { Component, Input } from '@angular/core';
+import { bounceOutLeftAnimation } from '../../animations';
 import { CategoryService } from '../../category.service';
 import { AppCategory } from '../../models/app-category';
 
@@ -6,6 +8,11 @@ import { AppCategory } from '../../models/app-category';
   selector: 'product-filter',
   templateUrl: './product-filter.component.html',
   styleUrls: ['./product-filter.component.css'],
+  animations: [
+    trigger('todosAnimation', [transition(':enter', group([query('@todoAnimation', stagger(100, animateChild()))]))]),
+
+    trigger('todoAnimation', [transition(':enter', [style({ opacity: 0 }), animate(300)]), transition(':leave', [style({ backgroundColor: 'tomato' }), animate(1000), useAnimation(bounceOutLeftAnimation)])]),
+  ],
 })
 export class ProductFilterComponent {
   @Input('category') category;
