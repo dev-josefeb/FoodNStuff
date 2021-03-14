e3d43a417a92f384ef6d977c2434788cd9aa63dc
dev-josefeb <febinjose@protonmail.com>
2021-03-14 14:00:28 +0100

    Injected cart into checkout component

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ShoppingCart } from '../models/shopping-cart';
+import { ShoppingCartService } from '../shopping-cart.service';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+  styleUrls: ['./checkout.component.css'],
 })
 export class CheckoutComponent implements OnInit {
+  cart$: Observable<ShoppingCart>;
 
-  constructor() { }
+  constructor(private cartService: ShoppingCartService) {}
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    this.cart$ = await this.cartService.getCart();
   }
-
 }
