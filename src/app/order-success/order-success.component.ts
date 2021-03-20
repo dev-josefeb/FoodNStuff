7cdadd016c138b8140021b995d9c41edea968f63
dev-josefeb <febinjose@protonmail.com>
2021-03-20 22:35:02 +0100

    Add order summary on order submit

--- src/app/order-success/order-success.component.ts
+++ src/app/order-success/order-success.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Order } from '../models/order';
+import { OrderService } from '../order.service';
 
 @Component({
   selector: 'app-order-success',
   templateUrl: './order-success.component.html',
-  styleUrls: ['./order-success.component.css']
+  styleUrls: ['./order-success.component.css'],
 })
-export class OrderSuccessComponent implements OnInit {
+export class OrderSuccessComponent {
+  id: string;
+  order: Order;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private orderService: OrderService, private route: ActivatedRoute) {
+    this.id = this.route.snapshot.paramMap.get('id');
+    if (this.id) this.orderService.getOrderById(this.id).subscribe((o: Order) => (this.order = o));
   }
-
 }
