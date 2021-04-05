40da8274b53d17d81b631c9bee68d4bd2b086435
dev-josefeb <febinjose@protonmail.com>
2021-04-05 17:34:35 +0200

    Fix missing auto fill properties when user redirected to the checkout page after signing up

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -7,8 +7,9 @@ import { Subscription } from 'rxjs';
 import { AuthService } from '../_services/auth.service';
 import { Order } from '../_models/order';
 import { Router } from '@angular/router';
-import firebase from 'firebase';
 import { Billing } from '../_models/billing';
+import { UserService } from '../_services/user.service';
+import firebase from 'firebase';
 
 @Component({
   selector: 'app-checkout',
@@ -16,20 +17,21 @@ import { Billing } from '../_models/billing';
   styleUrls: ['./checkout.component.css'],
 })
 export class CheckoutComponent implements OnInit, OnDestroy {
-  billing: Billing = { firstName: '', lastName: '', email: '', address: '', address2: '', cardName: '', cardNumber: '', cardExpiry: '', cardCvv: '', sameShippingAddress: false, country: '', city: '', zip: '' };
+  billing: Billing = { firstName: '', lastName: '', email: '', address: '', address2: '', cardName: '', cardNumber: '', cardExpiry: new Date(), cardCvv: '', sameShippingAddress: false, country: '', city: '', zip: '' };
   cart: ShoppingCart;
   countries: string[] = [];
   userId: string;
   cartSubscription: Subscription;
+  authUserSubscription: Subscription;
   userSubscription: Subscription;
 
-  constructor(private authService: AuthService, private cartService: ShoppingCartService, private countryService: CountryService, private orderService: OrderService, private router: Router) {}
+  constructor(private authService: AuthService, private cartService: ShoppingCartService, private countryService: CountryService, private orderService: OrderService, private router: Router, private userService: UserService) {}
 
   async ngOnInit() {
     let cart$ = await this.cartService.getCart();
     this.cartSubscription = cart$.subscribe((cart) => (this.cart = cart));
 
-    this.userSubscription = this.authService.user$.subscribe((user) => {
+    this.authUserSubscription = this.authService.user$.subscribe((user) => {
       this.userId = user.uid;
       this.setUserDetails(user);
     });
@@ -47,14 +49,16 @@ export class CheckoutComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.cartSubscription.unsubscribe();
+    this.authUserSubscription.unsubscribe();
     this.userSubscription.unsubscribe();
   }
 
   private setUserDetails(user: firebase.User) {
-    this.billing.email = user.email;
-
-    if (!user.displayName) return;
-    this.billing.firstName = user.displayName.split(' ')[0];
-    this.billing.lastName = user.displayName.split(' ')[1];
+    this.userSubscription = this.userService.get(user.uid).subscribe((user) => {
+      this.billing.email = user.email;
+      this.billing.firstName = user.name.split(' ')[0];
+      this.billing.lastName = user.name.split(' ')[1];
+      this.billing.cardName = user.name;
+    });
   }
 }
